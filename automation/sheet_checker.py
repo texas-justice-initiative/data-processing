1e08dbe2093f5847daaf67cd668c2c2a8cb11632
Aiden Yang <alyang250@gmail.com>
2018-11-24 17:25:17 -0600

    Adding docstrings

--- automation/sheet_checker.py
+++ automation/sheet_checker.py
@@ -1,3 +1,5 @@
+"""Summary
+"""
 import os
 import sys
 import pytz
@@ -17,7 +19,24 @@ from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 
 
 class SheetChecker(object):
-    def __init__(self, dataset, emailer, sheet_key, cleaning_nbs, force, sync):
+
+    """ Sheet Checker is intended to run as a cronjob. It checks the last update timestamp of a google sheet against
+    the cronjob's last run timestamp. If the sheet has been updated since the last run, it runs the cleaning notebooks
+    and compression notebooks, then updates the timestamp. 
+    """
+    
+    def __init__(self, dataset, emailer, sheet_key, cleaning_nbs, compression_nbs, force, sync):
+        """ Constructor for Sheet Checker object. 
+
+        Args:
+            dataset (string): Name of dataset
+            emailer (TJIEmailer): Emailer obj used for sending success and failures emails through SES 
+            sheet_key (string): Google Sheets key
+            cleaning_nbs ([] of strings): List of cleaning notebooks to run
+            compression_nbs ([] of strings): List of compression notebooks to run
+            force (boolean): Controls whether or not to run cleaning and compression nbs even if sheet has not been updated
+            sync (bolean): Controls whether or not notebooks will sync data to Data.world
+        """
         self.s3_client = boto3.client('s3')
         self.emailer = emailer
         self.force_full_update = force
@@ -25,9 +44,10 @@ class SheetChecker(object):
         self.dataset = dataset
         self.sheet_key = sheet_key
         self.cleaning_nbs = cleaning_nbs
+        self.compression_nbs = compression_nbs
         self.logger = logging.getLogger(dataset)
         timestamp = datetime.now().strftime('%Y-%m-%d')
-        logging.basicConfig(filename='%s+%s.log' % (self.dataset, timestamp), level=logging.INFO)
+        logging.basicConfig(filename='logs/%s+%s.log' % (self.dataset, timestamp), level=logging.INFO)
 
     def run(self):
         if self.is_sheet_updated() or self.force_full_update:
@@ -42,6 +62,8 @@ class SheetChecker(object):
             self.update_last_ran_ts()
 
     def clean(self):
+        """ Runs cleaning notebooks
+        """
         try:
             for cleaning_nb in self.cleaning_nbs:
                 self.run_cleaning_notebook(cleaning_nb)
@@ -53,8 +75,11 @@ class SheetChecker(object):
             sys.exit('Exiting: encountered an issue while cleaning.')
 
     def compress(self):
+        """ Runs compression notebooks
+        """
         try:
-            self.run_compression_notebook()
+            for compression_nb in self.compression_nbs:
+                self.run_compression_notebook(compression_nb)
             self.emailer.send_email(is_success=True, action="Compressing", dataset=self.dataset)
         except Exception as e:
             self.logger.exception(e)
@@ -64,21 +89,37 @@ class SheetChecker(object):
         self.logger.info("Successfully cleaned and compressed data.")
 
     def run_cleaning_notebook(self, cleaning_nb_name):
-        out_notebook_name = 'cleaning_%s_result_nb.ipynb' % self.dataset
+        """Runs a single cleaning notebook
+        
+        Args:
+            cleaning_nb_name (string): File name of cleaning notebook in data_cleaning directory
+        """
+        out_notebook_name = 'output_notebooks/cleaning_%s_result_nb.ipynb' % self.dataset
         nb = nbformat.read('../data_cleaning/%s' % cleaning_nb_name, as_version=4)
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
         self.run_notebook(ep, nb, out_notebook_name)
         self.logger.info("Successfully cleaned.")
 
     def run_compression_notebook(self, compression_nb_name):
-        out_notebook_name = 'compression_%s_result_nb.ipynb' % self.dataset
+        """Runs a single compression notebook
+        
+        Args:
+            compression_nb_name (TYPE): File name of a compression notebook in data_cleaning directory
+        """
+        out_notebook_name = 'output_notebooks/compression_%s_result_nb.ipynb' % self.dataset
         nb = nbformat.read('../data_cleaning/%s' % compression_nb_name, as_version=4)
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
         self.run_notebook(ep, nb, out_notebook_name)
         self.logger.info("Successfully compressed.")
-        return True
 
     def run_notebook(self, ep, nb, out_notebook_name):
+        """Runs a notebook and write out the output notebook
+        
+        Args:
+            ep (ExecutePreprocessor): ExecutePreprocessor instance
+            nb (NotebookNode): NotebookNode instance
+            out_notebook_name (string): Where to write output notebook
+        """
         try:
             ep.preprocess(nb, {'metadata': {'path': '../data_cleaning/'}})
         except CellExecutionError:
@@ -91,6 +132,11 @@ class SheetChecker(object):
                 nbformat.write(nb, f)
 
     def is_sheet_updated(self):
+        """Summary
+        
+        Returns:
+            TYPE: Description
+        """
         sheet_last_updated_ts = dateutil.parser.parse(self.get_sheet_update_ts())
         last_run_ts = self.fetch_last_run_ts()
         if not last_run_ts:
@@ -101,6 +147,10 @@ class SheetChecker(object):
             return sheet_last_updated_ts >= job_last_run_ts
 
     def get_sheet_update_ts(self):
+        """
+        Returns:
+            last_updated_ts: Last update timestamp of google sheet
+        """
         gc = pygsheets.authorize(service_file='client_secret.json')
         gc.enableTeamDriveSupport = True
         sheet = gc.open_by_key(self.sheet_key)
@@ -108,6 +158,8 @@ class SheetChecker(object):
         return last_updated_ts
 
     def update_last_ran_ts(self):
+        """Drops current time as timestamp in s3, to be used next time this job runs
+        """
         timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
         buff = BytesIO()
         buff.write(timestamp.encode('utf-8'))
@@ -116,6 +168,11 @@ class SheetChecker(object):
         self.logger.info("Updated %s timestamp." % self.dataset)
 
     def fetch_last_run_ts(self):
+        """Fetches last job run timestamp from s3
+        
+        Returns:
+            last_run_ts: Last time this job ran
+        """
         try:
             timestamp = self.s3_client.get_object(Bucket='tji-timestamps', Key=self.dataset)
             body = timestamp['Body']
@@ -129,6 +186,8 @@ class SheetChecker(object):
                 self.logger.info("Something went wrong while fetching timestamp.")
 
     def set_up_environment(self):
+        """Sets environment variables that controls whether or not notebooks write to data.world and/or s3
+        """
         dataset = self.dataset.upper()
         if self.sync_dw:
             os.environ['CLEAN_%s_DW' % dataset] = 'TRUE'
@@ -136,6 +195,8 @@ class SheetChecker(object):
         os.environ['COMPRESS_%s_S3' % dataset] = 'TRUE'
 
     def clean_up_environment(self):
+        """Unsets all environment variables
+        """
         dataset = self.dataset.upper()
         os.unsetenv('CLEAN_%s_DW' % dataset)
         os.unsetenv('CLEAN_%s_S3' % dataset)
@@ -147,6 +208,7 @@ if __name__ == '__main__':
     parser.add_argument('-force', action='store_true', help='Force everything to re-clean and re-compress')
     parser.add_argument('-sync', action='store_true', help='Sync to data.world')
     args = parser.parse_args()
+
     # Parse config.yaml
     with open('config.yaml') as f:
         config = yaml.load(f)
@@ -154,14 +216,17 @@ if __name__ == '__main__':
     # Set up emailer obj
     email_config = config['Email Settings']
     emailer = TJIEmailer(sender=email_config['sender'],
-                             recipients=email_config['recipients'],
-                             aws_region=email_config['region'])
+                         recipients=email_config['recipients'],
+                         aws_region=email_config['region'])
 
+    # Create and run sheet checker for one dataset
     for dataset, settings in config['Datasets'].iteritems():
-        sc = SheetChecker(dataset=dataset,
-                          emailer=emailer,
-                          sheet_key=settings['sheet_key'],
-                          cleaning_nbs=settings['cleaning_notebooks'],
-                          force=args.force,
-                          sync=args.sync)
-        sc.run()
+        if settings['enabled']:
+            sc = SheetChecker(dataset=dataset,
+                              emailer=emailer,
+                              sheet_key=settings['sheet key'],
+                              cleaning_nbs=settings['cleaning notebooks'],
+                              compression_nbs=settings['compressions notebooks']
+                              force=args.force,
+                              sync=args.sync)
+            sc.run()
